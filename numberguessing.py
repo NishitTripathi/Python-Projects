94c7f48cf086ff6d520a03d7df93a15806b87a56
Nishit Tripathi <nishittripathi@Nishits-MacBook-Pro.local>
2026-01-02 07:09:26 +0530

    Secondary commit

--- numberguessing.py
+++ numberguessing.py
@@ -3,13 +3,17 @@ print ("Welcome to number guessing game!!!")
 number = random.randint(1,100)
 
 while True:
-    guess = int(input("Enter a number between 1 to 100: "))
-    if guess == number:
-        print("Congratulations! You guessed the number!")
-        break
-    elif guess > number:
-        print("Too High!")
-    elif guess < number:
-        print("Too Low!")
-    else:
-        print("Please enter a valid number")
+    try:
+        guess = int(input("Guess the number between 1 and 100 "))
+        if guess > number:
+            print("Too High!")
+        elif guess < number:
+            print("Too Low!")
+        else:
+            print("Congratulations! You guessed the number.")
+            break
+    except ValueError:
+        print("Please enter a valid input")
+        
+
+        
