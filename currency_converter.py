4a723d49b123f6e1acd5266a40888df47f31937b
Nishit Tripathi <nishittripathi007@gmail.com>
2026-01-03 00:16:16 +0530

    Secondary commit currency converter

--- currency_converter.py
+++ currency_converter.py
@@ -1,31 +1,42 @@
-amount = int(input("Enter the amount "))
-source = input("Enter the source currency: (USD/INR/GBP)").lower()
-target = input("Enter the target currency (USD/INR/GBP)").lower()
+def get_amount():
+  while True:
+    try:
+      amount = float(input('Enter the amount: '))
+      if amount <= 0:
+        raise ValueError()
+      return amount
+    except ValueError:
+      print('Invalid amount')
 
-if source == "usd" and target == "inr":
-    value = (amount * 90.9)
-    print(value)
+def get_currency(label):
+  currencies = ('USD', 'EUR', 'CAD')
+  while True:
+    currency = input(f'{label} currency (USD/EUR/CAD): ').upper()
+    if currency not in currencies:
+      print('Invalid currency')
+    else:
+      return currency
 
-elif source == "inr" and target == "usd":
-    value = (amount/90.9)
-    print(value)
-    
-elif source == "gbp" and target == "inr":
-    value = (amount * 121.26)
-    print(value)
 
-elif source == "inr" and target == "gbp":
-    value = (amount/121.26)
-    print(value)
+def convert(amount, source_currency, target_currency):
+  exchange_rates = {
+    'USD': { 'EUR': 0.85, 'CAD': 1.25 },
+    'EUR': { 'USD': 1.18, 'CAD': 1.47 },
+    'CAD': { 'USD': 0.80, 'EUR': 0.68 },
+  }
 
-elif source == "gbp" and target == "usd":
-    value = (amount * 1.35)
-    print(value)
-    
-elif source == "usd" and target == "gbp":
-    value = (amount/1.25)
-    print(value)
-    
-else:
-    print("Enter valid input")
+  if source_currency == target_currency:
+    return amount
+  
+  return amount * exchange_rates[source_currency][target_currency]
 
+def main():
+  amount = get_amount()
+  source_currency = get_currency('Source')
+  target_currency = get_currency('Target')
+  converted_amount = convert(amount, source_currency, target_currency)
+  print(f'{amount} {source_currency} is equal to {converted_amount:.2f}')
+
+
+if __name__ == "__main__":
+  main()
